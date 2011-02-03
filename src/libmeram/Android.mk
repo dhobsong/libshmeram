c3f4dae67760d426034d90a55e042f096535cac6
Damian Hobson-Garcia <dhobsong@igel.co.jp>
2011-02-03 12:36:22 +0900

    Add Android.mk files for Android compile

---
+++ src/libmeram/Android.mk
@@ -0,0 +1,18 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_C_INCLUDES := \
+	external/libmeram/include \
+	external/libuiomux/include \
+
+LOCAL_CFLAGS := -DCONFIG_FILE=\"/system/etc/meram.conf\"
+
+LOCAL_SRC_FILES := \
+	meram.c \
+	ipmmui.c
+
+LOCAL_SHARED_LIBRARIES := libcutils libuiomux
+
+LOCAL_MODULE := libmeram
+LOCAL_PRELINK_MODULE := false
+include $(BUILD_SHARED_LIBRARY)
