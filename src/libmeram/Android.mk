bb749b88943e91da7c29327f9551c47fecae8931
Katsuya MATSUBARA <matsu@igel.co.jp>
2011-04-07 15:22:54 +0900

    android: use relative path description in Android.mk
    It must be useful for moving location of libmeram in the android tree.

--- src/libmeram/Android.mk
+++ src/libmeram/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_C_INCLUDES := \
-	external/libmeram/include \
+	$(LOCAL_PATH)/../../include \
 	external/libuiomux/include \
 
 LOCAL_CFLAGS := -DCONFIG_FILE=\"/system/etc/meram.conf\"
